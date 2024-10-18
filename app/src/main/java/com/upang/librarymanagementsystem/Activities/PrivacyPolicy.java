7ca07bfbd33ddedbe6d8ad40f03ba9113c094c15
Ralph Luis Mamaril <rlmamaril58@gmail.com>
2024-10-18 21:43:28 +0800

    buttons connected

--- app/src/main/java/com/upang/librarymanagementsystem/Activities/PrivacyPolicy.java
+++ app/src/main/java/com/upang/librarymanagementsystem/Activities/PrivacyPolicy.java
@@ -1,6 +1,10 @@
 package com.upang.librarymanagementsystem.Activities;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.upang.librarymanagementsystem.R;
 
 public class PrivacyPolicy extends AppCompatActivity {
+    ImageButton btnBackPrivacy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,17 @@ public class PrivacyPolicy extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnBackPrivacy = findViewById(R.id.btnBackPrivacy);
+
+        btnBackPrivacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrivacyPolicy.this, Settings.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
--- app/src/main/java/com/upang/librarymanagementsystem/Activities/QRCode.java
+++ app/src/main/java/com/upang/librarymanagementsystem/Activities/QRCode.java
@@ -1,6 +1,10 @@
 package com.upang.librarymanagementsystem.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.upang.librarymanagementsystem.R;
 
 public class QRCode extends AppCompatActivity {
+    ImageButton btnBackQR;
+    Button btnCreateQR;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -18,8 +25,27 @@ public class QRCode extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets
-                    ;
+            return insets;
+        });
+
+        btnBackQR = findViewById(R.id.btnBackQR);
+
+        btnBackQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QRCode.this, AccountPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnCreateQR = findViewById(R.id.btnCreateQR);
+
+        btnCreateQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 
